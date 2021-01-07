f7771f5d99a4add5c242fbee2046009199d01cc7
Bastien Guyl <bguyl@smart-tale.com>
2021-01-07 17:43:25 +0100

    Commit scene bug

---
+++ Assets/BasicSO.cs
@@ -0,0 +1,21 @@
+﻿namespace Scenes
+{
+	using System;
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = "BasicSO", menuName = "BasicSO", order = 0)]
+	public class BasicSO : ScriptableObject
+	{
+		private void OnEnable()
+		{
+			hideFlags = HideFlags.DontUnloadUnusedAsset;
+		}
+
+		public float value = 0f;
+
+		public void Add()
+		{
+			value += 1f;
+		}
+	}
+}
---
+++ Assets/GotoScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoScene : MonoBehaviour
+{
+    public void GoTo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
