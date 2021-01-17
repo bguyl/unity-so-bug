2d695ccdfe08b31187aa1d38ef387d33ad8bc00b
Bastien Guyl <bguyl@smart-tale.com>
2021-01-17 13:58:27 +0100

    Simplify issue visalization

---
+++ Assets/Scripts/DisplaySOValue.cs
@@ -0,0 +1,32 @@
+﻿using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Show either Atom or Vanilla SO float value
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class DisplaySOValue : MonoBehaviour
+{
+	[Header("Show Atom value OR Vanilla value")]
+	[SerializeField] private FloatVariable _atomSO;
+	[SerializeField] private VanillaFloatSO _vanillaSO;
+	private Text _text;
+
+	private void Awake()
+	{
+		_text = GetComponent<Text>();
+	}
+
+	private void LateUpdate()
+	{
+		if (_text != null && _atomSO != null)
+		{
+			_text.text = _atomSO.Value.ToString();	
+		}
+		else if (_text != null && _vanillaSO != null)
+		{
+			_text.text = _vanillaSO.Value.ToString();	
+		}
+	}
+}
---
+++ Assets/Scripts/SceneName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Simply display SceneName
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class SceneName : MonoBehaviour
+{
+	private void Start()
+	{
+		GetComponent<Text>().text = SceneManager.GetActiveScene().name;
+	}
+}
